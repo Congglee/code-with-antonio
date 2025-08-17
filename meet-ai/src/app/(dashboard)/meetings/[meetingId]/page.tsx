c70e4cc80bed9b4ffd3a87cff92dfed85f64186c
Congglee <congldqn888@gmail.com>
2025-08-18 01:51:17 +0700

    meet-ai - meetings setup

---
+++ meet-ai/src/app/(dashboard)/meetings/[meetingId]/page.tsx
@@ -0,0 +1,3 @@
+export default function MeetingDetails() {
+  return <div>MeetingDetails</div>;
+}
--- meet-ai/src/trpc/routers/_app.ts
+++ meet-ai/src/trpc/routers/_app.ts
@@ -1,8 +1,10 @@
 import { agentsRouter } from "@/modules/agents/server/procedures";
+import { meetingsRouter } from "@/modules/meetings/server/procedures";
 import { createTRPCRouter } from "@/trpc/init";
 
 export const appRouter = createTRPCRouter({
   agents: agentsRouter,
+  meetings: meetingsRouter,
 });
 
 // Export type definition of API
