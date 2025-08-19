4dd001da84e9b4783fc406cd40bd495aecb203e7
Congglee <congldqn888@gmail.com>
2025-08-19 10:36:37 +0700

    meet-ai - completed state

--- meet-ai/src/modules/meetings/ui/components/columns.tsx
+++ meet-ai/src/modules/meetings/ui/components/columns.tsx
@@ -2,11 +2,10 @@
 
 import GeneratedAvatar from "@/components/generated-avatar";
 import { Badge } from "@/components/ui/badge";
-import { cn } from "@/lib/utils";
+import { cn, formatDuration } from "@/lib/utils";
 import { MeetingGetMany } from "@/modules/meetings/types";
 import { ColumnDef } from "@tanstack/react-table";
 import { format } from "date-fns";
-import humanizeDuration from "humanize-duration";
 import {
   CircleCheckIcon,
   CircleXIcon,
@@ -15,14 +14,6 @@ import {
   CornerDownRightIcon,
   LoaderIcon,
 } from "lucide-react";
-
-function formatDuration(seconds: number) {
-  return humanizeDuration(seconds * 1000, {
-    largest: 1,
-    round: true,
-    units: ["h", "m", "s"],
-  });
-}
 
 const statusIconMap = {
   upcoming: ClockArrowUpIcon,
